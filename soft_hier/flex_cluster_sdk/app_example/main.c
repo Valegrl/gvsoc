20aba99775eca186794c23cafb2e9b123548c86c
Valegrl <valerio.grillo@mail.polimi.it>
2026-03-13 17:38:31 +0000

    simple main runtime

--- soft_hier/flex_cluster_sdk/app_example/main.c
+++ soft_hier/flex_cluster_sdk/app_example/main.c
@@ -5,21 +5,21 @@
 int main()
 {
     uint32_t eoc_val = 0;
-    flex_barrier_xy_init();
-    flex_global_barrier_xy();
-    if (flex_get_core_id() == 0 && flex_get_cluster_id() == 0) flex_timer_start();
-    /**************************************/
-    /*  Program Execution Region -- Start */
-    /**************************************/
+    // flex_barrier_xy_init();
+    // flex_global_barrier_xy();
+    // if (flex_get_core_id() == 0 && flex_get_cluster_id() == 0) flex_timer_start();
+    // /**************************************/
+    // /*  Program Execution Region -- Start */
+    // /**************************************/
 
-    // example_vector_add();
+    // // example_vector_add();
 
-    /**************************************/
-    /*  Program Execution Region -- Stop  */
-    /**************************************/
-    flex_global_barrier_xy();
-    if (flex_get_core_id() == 0 && flex_get_cluster_id() == 0) flex_timer_end();
-    flex_global_barrier_xy();
+    // /**************************************/
+    // /*  Program Execution Region -- Stop  */
+    // /**************************************/
+    // flex_global_barrier_xy();
+    // if (flex_get_core_id() == 0 && flex_get_cluster_id() == 0) flex_timer_end();
+    // flex_global_barrier_xy();
     flex_eoc(eoc_val);
     return 0;
 }
